1ffd5dfca9b7a6b3e34a07d85cb6dce95fbdf0ea
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-12-03 04:47:30 +0300

    /app.js express-fileupload commit

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
-const express = require('express');
-const fileUpload = require('express');
+const express = require("express");
+const fileUpload = require("express-fileupload");
 const app = express();
 
 const PORT = 8000;
-app.use('/form', express.static(__dirname + '/index.html'));
+app.use("/form", express.static(__dirname + "/index.html"));
 
 // default options
-// app.use(fileUpload());
+app.use(fileUpload());
 
-app.get('/', function(req, res) {
-  res.send('ricky_sensei');
+app.get("/", function (req, res) {
+  res.send("ricky_sensei");
 });
 
-app.post('/upload', (req, res) =>{
-  console.log( req.body); // ricky has bugs
+app.post("/upload", (req, res) => {
   let sampleFile;
   let uploadPath;
 
   if (!req.files || Object.keys(req.files).length === 0) {
-    res.status(400).send('No files were uploaded.');
+    res.status(400).send("No files were uploaded.");
     return;
   }
 
-  console.log('req.files >>>', req.files); 
+  console.log("req.files >>>", req.files);
 
   sampleFile = req.files.sampleFile;
 
-  uploadPath = __dirname + '/uploads/' + sampleFile.name;
+  uploadPath = __dirname + "/uploads/" + sampleFile.name;
 
-  sampleFile.mv(uploadPath, function(err) {
+  sampleFile.mv(uploadPath, function (err) {
     if (err) {
       return res.status(500).send(err);
     }
 
-    res.send('File uploaded to ' + uploadPath);
+    res.send("File uploaded to " + uploadPath);
   });
 });
 
-app.listen(PORT, function() {
-  console.log('Express server listening on port ', PORT); // eslint-disable-line
+app.listen(PORT, function () {
+  console.log("Express server listening on port ", PORT); // eslint-disable-line
 });
